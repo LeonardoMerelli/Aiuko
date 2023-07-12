fc4193960584f9431401bdf2ba4a1864164197db
Leonardo Merelli <s1100073@studenti.univpm.it>
2023-07-12 16:41:35 +0200

    inizio della home

---
+++ resources/views/body.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aiuko</title>
+
+    <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}"> 
+</head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('body')
+
+@section('content')
+    <div class="sfondo_container">
+        <div class="sfondo_iniziale"></div>
+        <div class="sfumatura"></div>
+        <div class="titolo"></div>
+    </div>
+    <div class="accesso_container"></div>
+
+@endsection
