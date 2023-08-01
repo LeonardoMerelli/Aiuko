2658eb0022662843781f4d32e641a62ecf5cf9dc
Leonardo Merelli <s1100073@studenti.univpm.it>
2023-08-01 10:26:03 +0200

    caso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,10 @@
         <div class="sfumatura"></div>
         <div class="titolo"></div>
     </div>
-    <div class="accesso_container"></div>
+    <div class="accesso_container">
+        <a href="#">Sign up with email</a>
+        <span>Already have an account?</span>
+        <a href="#">Log In</a>
+    </div>
 
 @endsection
