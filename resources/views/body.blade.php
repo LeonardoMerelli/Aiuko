217bac59bca8bb953788ebd8eea48f58c52e631a
Tommaso <s1100538@studenti.univpm.it>
2023-08-01 16:49:34 +0200

    Add first page

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aiuko</title>
 
+    <link rel="stylesheet" type="text/css" href="{{asset('css/body.css')}}"> 
     <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}"> 
 </head>
 <body>
