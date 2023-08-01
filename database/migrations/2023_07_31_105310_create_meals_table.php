73f71f1fe201b2a12495ab1ec04e60f699e22eec
Leonardo Merelli <s1100073@studenti.univpm.it>
2023-08-01 10:14:34 +0200

    migrations

--- database/migrations/2023_07_31_105310_create_meals_table.php
+++ database/migrations/2023_07_31_105310_create_meals_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('meals', function (Blueprint $table) {
+            $table->bigIncrements('idPasto');
+            $table->string('nome');
+            $table->string('immagine');
+            $table->string('descrizione');
+            $table->string('dieta');
+            $table->string('tempo');
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('meals');
     }
 };
