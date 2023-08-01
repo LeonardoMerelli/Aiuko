2179c5fd98bdaa746cafe03bd8d55ba61307d8a2
Tommaso <s1100538@studenti.univpm.it>
2023-08-01 16:51:24 +0200

    add first page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,13 +34,5 @@
     </div>
 
 </div>
-<!--
-    <div class="sfondo_container">
-            <div class="sfondo_iniziale"></div>
-            <div class="sfumatura"></div>
-            <div class="titolo"></div>
-        </div>
-        <div class="accesso_container"></div>
--->
 
 @endsection
