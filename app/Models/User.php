3395c543813eeb271013e2aa154dcfd4b94400ae
Nicola <s1099740@studenti.univpm.it>
2023-08-04 12:42:07 +0200

    social access

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,8 @@ class User extends Authenticatable
         'citta',
         'email',
         'password',
+        'google_id',
+        'facebook_id',
     ];
 
     protected $hidden = [
--- config/services.php
+++ config/services.php
@@ -34,7 +34,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT'),
+        'redirect' => 'http://127.0.0.1:8000/Aiuko/auth/google/call-back',
+    ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => 'https://localhost:8000/Aiuko/auth/facebook/call-back',
     ],
 
 ];
