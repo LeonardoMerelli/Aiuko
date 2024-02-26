42d00a31167d41d50bb0c0294a0d096d8c36df80
Leonardo Merelli <s1100073@studenti.univpm.it>
2024-02-26 19:14:58 +0100

    elettrodomestici x nico

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/menu.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/caratteristiche.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/snack.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/elettrodomestici.css')}}">
 
     <link href="https://fonts.cdnfonts.com/css/believer2" rel="stylesheet">
                 
