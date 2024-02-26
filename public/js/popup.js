f3caec03d5567b79bcfe69a67c52ad4cc20f9bfb
Leonardo Merelli <s1100073@studenti.univpm.it>
2024-02-26 17:52:53 +0100

    snack

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,13 +1,23 @@
+document.querySelector('.confirm-button').classList.add("disabled");
+
 function showPopup() {
-  var popup = document.getElementById('popup');
-  if (popup) {
-      popup.style.display = 'block';
-  }
+  document.querySelector('.select-item-yes').classList.add('selected-yes');
+    
+  document.querySelector('.select-item-no').classList.remove('selected-no');
+
+  document.getElementById('popup').style.display = 'block';
+
+  document.querySelector('.confirm-button').disabled = false;
+  document.querySelector('.confirm-button').classList.remove("disabled");
 }
 
 function closePopup() {
-  var popup = document.getElementById('popup');
-  if (popup) {
-      popup.style.display = 'none';
-  }
+  document.querySelector('.select-item-no').classList.add('selected-no');
+  
+  document.querySelector('.select-item-yes').classList.remove('selected-yes');
+
+  document.getElementById('popup').style.display = 'none';
+
+  document.querySelector('.confirm-button').disabled = false;
+  document.querySelector('.confirm-button').classList.remove("disabled");
 }
--- resources/views/snack.blade.php
+++ resources/views/snack.blade.php
@@ -10,22 +10,19 @@
   <span><strong>PASTI</strong></span>
   <h5>FAI SNACK DURANTE IL GIORNO</h5>
   <div class="select-div">
-    <div class="select-item selected">
-      <div class="select-item" onclick="showPopup()">
+      <div class="select-item-yes" onclick="showPopup()">
         <div>
           <svg width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="tick"> <polyline fill="none" points="3.7 14.3 9.6 19 20.3 5" stroke="#2cc92f" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline> </g> </g> </g></svg>
         </div>
         SI
       </div>
-    </div>
-    <div class="select-item selected">
-      <div class="select-item" onclick="closePopup()">
+
+      <div class="select-item-no" onclick="closePopup()">
         <div>
           <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 5L4.99998 19M5.00001 5L19 19" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
         </div>
         NO
       </div>
-    </div>
   </div>
   <div id="popup" class="popup">
     <p>Quando desideri fare lo snack?</p>
