c496bf4cdde7e2dca608d3064e802888aabd91b5
Nicola <s1099740@studenti.univpm.it>
2024-02-26 16:42:38 +0100

    fix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,5 +31,13 @@ class AuthServiceProvider extends ServiceProvider
             }
             return false;
         });
+
+        Gate::define('isNotRegistered', function ($user) {
+            if($user->cognome === null and $user->citta === null and $user->password === null) {
+                return true;
+            }
+            return false;
+        });
+
     }
 }
