dc89781448963d4992c3260ad7de8e8cf74a0b6a
Leonardo Merelli <s1100073@studenti.univpm.it>
2024-02-26 11:32:41 +0100

    stile caratt

--- resources/views/caratteristicheuser.blade.php
+++ resources/views/caratteristicheuser.blade.php
@@ -42,6 +42,9 @@
                   </ul>
                   @endif
           </div>
+
+          <span>Livello di attività giornaliera</span>
+          
           <div class= "radio-div">
 
           @if($user->attivitaGiornaliera == null)
@@ -66,56 +69,9 @@
                   {{Form::label('inLineRadio1','Molto intensa')}}
               </div>
           @endif
+          </div>    
 
-              <div class="radio-label">
-               @if($user->attivitaGiornaliera == "sedentario")
-                  {{Form::radio('attivita','sedentario',true)}}
-                  {{Form::label('inLineRadio1','Sedentario')}}
-                @else
-                  {{Form::radio('attivita','sedentario')}}
-                  {{Form::label('inLineRadio1','Sedentario')}}
-                @endif
-              </div>
-              <div class="radio-label">
-               @if($user->attivitaGiornaliera == "leggera")
-                  {{Form::radio('attivita','leggera', true)}}
-                  {{Form::label('inLineRadio1','Leggera')}}
-                @else
-                  {{Form::radio('attivita','leggera')}}
-                  {{Form::label('inLineRadio1','Leggera')}}
-                @endif
-              </div>
-              <div class="radio-label">
-               @if($user->attivitaGiornaliera == "moderata")
-                  {{Form::radio('attivita','moderata', true)}}
-                  {{Form::label('inLineRadio1','Moderata')}}
-                @else
-                  {{Form::radio('attivita','moderata')}}
-                  {{Form::label('inLineRadio1','Moderata')}}
-                @endif
-              </div>
-              <div class="radio-label">
-               @if($user->attivitaGiornaliera == "intensa")
-                  {{Form::radio('attivita','intensa', true)}}
-                  {{Form::label('inLineRadio1','Intensa')}}
-                @else
-                  {{Form::radio('attivita','intensa')}}
-                  {{Form::label('inLineRadio1','Intensa')}}
-                @endif
-              </div>
-              <div class="radio-label">
-               @if($user->attivitaGiornaliera == "moltointensa")
-                  {{Form::radio('attivita','moltointensa', true)}}
-                  {{Form::label('inLineRadio1','Molto intensa')}}
-                @else
-                  {{Form::radio('attivita','moltointensa')}}
-                  {{Form::label('inLineRadio1','Molto intensa')}}
-                @endif
-              </div>
-
-
-          </div>
-
+          <span>Genere</span>
           @if($user->genere == null)
           <div class= "radio-div">
                 <div class="radio-label">
@@ -131,38 +87,7 @@
                     {{Form::label('inLineRadio1','Altro')}}
                 </div>
           </div>
-          @endif
-
-          <div class= "radio-div">
-                <div class="radio-label">
-                @if($user->genere == "uomo")
-                    {{Form::radio('genere','uomo',true)}}
-                    {{Form::label('inLineRadio1','Uomo')}}
-                @else
-                    {{Form::radio('genere','uomo')}}
-                    {{Form::label('inLineRadio1','Uomo')}}
-                @endif
-                </div>
-                <div class="radio-label">
-                @if($user->genere == "donna")
-                    {{Form::radio('genere','donna', true)}}
-                    {{Form::label('inLineRadio1','Donna')}}
-                @else
-                    {{Form::radio('genere','donna')}}
-                    {{Form::label('inLineRadio1','Donna')}}
-                @endif
-                </div>
-                <div class="radio-label">
-                @if($user->genere == "altro")
-                    {{Form::radio('genere','altro', true)}}
-                    {{Form::label('inLineRadio1','Altro')}}
-                @else
-                    {{Form::radio('genere','altro')}}
-                    {{Form::label('inLineRadio1','Altro')}}
-                @endif
-                </div>
-          </div>
-
+          @endif              
           <div class="input-container-btn-div">
               <a href="{{route('intollerances.create')}}" class="torna-indietro">Indietro</a>
               {{ Form::submit('Avanti',['class'=>'input-container-btn'])}} 
