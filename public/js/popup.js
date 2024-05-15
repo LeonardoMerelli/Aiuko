314c38d1f3ce699965125648fb6bf0258687e911
Leonardo Merelli <leonardo.merelli@gmail.com>
2024-05-15 19:31:51 +0200

    coalzione

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,21 +1,21 @@
 document.querySelector('.confirm-button').classList.add("disabled");
 
 function showPopup() {
+  document.querySelector('.confirm-button').classList.remove("disabled");
+
   document.querySelector('.select-item-yes').classList.add('selected-yes');
     
   document.querySelector('.select-item-no').classList.remove('selected-no');
 
   document.getElementById('popup').style.display = 'block';
-
-  document.querySelector('.confirm-button').classList.remove("disabled");
 }
 
 function closePopup() {
+  document.querySelector('.confirm-button').classList.remove("disabled");
+
   document.querySelector('.select-item-no').classList.add('selected-no');
   
   document.querySelector('.select-item-yes').classList.remove('selected-yes');
 
   document.getElementById('popup').style.display = 'none';
-
-  document.querySelector('.confirm-button').classList.remove("disabled");
 }
