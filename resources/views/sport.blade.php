28068b4992de86876816717269e1854f78b5fa2c
Leonardo Merelli <leonardo.merelli@gmail.com>
2024-05-16 16:31:49 +0200

    fix tasto indietro

--- resources/views/sport.blade.php
+++ resources/views/sport.blade.php
@@ -97,7 +97,7 @@
         <svg viewBox="0 0 24 24" fill="none" height="50px" width="50px" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M12 4V20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
     </div>
     <div class="bottoni">
-      <a href="{{route('caratteristiche.create')}}">INDIETRO</a>
+      <a href="{{route('cena.create')}}">INDIETRO</a>
       <input type="submit" class="confirm-button" value="AVANTI">
     </div>
   </form>
